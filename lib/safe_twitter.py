5380fbf29069987340844ec298fe6ca09b71d392
Amit <amit.trivizki@gmail.com>
2016-10-12 00:04:46 +0300

    add ctor and prints to sate tweet

--- lib/safe_twitter.py
+++ lib/safe_twitter.py
@@ -3,60 +3,67 @@ from tweet import Tweet
 
 class SafeTwitter(object):
 
+    def __init__(self, min_ratelimit_search, min_ratelimit_post):
+        self.min_search_rate_limit = min_ratelimit_search
+        self.min_post_rate_limit = min_ratelimit_post
+
     def _check_error( self, r ):
-	r = r.json()
-	if 'errors' in r:
-	    print("We got an error message: " + r['errors'][0]['message'] + " Code: " + str(r['errors'][0]['code']) )
-	    #sys.exit(r['errors'][0]['code'])
-            return True
-        return False
-    
+    	r = r.json()
+    	if 'errors' in r:
+    	    print("We got an error message: " + r['errors'][0]['message'] + " Code: " + str(r['errors'][0]['code']) )
+    	    #sys.exit(r['errors'][0]['code'])
+                return True
+            return False
+
     def _check_rate_limit_search(self):
-	
-	r = api.request('application/rate_limit_status').json()
+    	r = api.request('application/rate_limit_status').json()
 
-	for res_family in r['resources']:
-	    for res in r['resources'][res_family]:
-		limit = r['resources'][res_family][res]['limit']
-		remaining = r['resources'][res_family][res]['remaining']
-		prcent = float(remaining)/float(limit)*100
+    	for res_family in r['resources']:
+    	    for res in r['resources'][res_family]:
+    		limit = r['resources'][res_family][res]['limit']
+    		remaining = r['resources'][res_family][res]['remaining']
+    		prcent = float(remaining)/float(limit)*100
 
-		if res == "/search/tweets":
-		    if precent < self.min_search_rate_limit:
-                        time.sleep(30)
-                        _check_rate_limit_search()
+    		if res == "/search/tweets":
+    		    if precent < self.min_search_rate_limit:
+                    print "search rate limit too low: ", precent, " want: ", self.min_search_rate_limit, " waiting 30 sec"
+                    time.sleep(30)
+                    self._check_rate_limit_search()
 
-                            
     def _check_rate_limit_post(self):
         r = api.request('application/rate_limit_status').json()
 
-	for res_family in r['resources']:
-	    for res in r['resources'][res_family]:
-		limit = r['resources'][res_family][res]['limit']
-		remaining = r['resources'][res_family][res]['remaining']
-		prcent = float(remaining)/float(limit)*100
-                
-            if res == "/application/rate_limit_status":
-                if precent < self.min_post_rate_limit:
-                    time.sleep(30)
-                    _check_rate_limit_post()
+    	for res_family in r['resources']:
+    	    for res in r['resources'][res_family]:
+    		    limit = r['resources'][res_family][res]['limit']
+    		    remaining = r['resources'][res_family][res]['remaining']
+    		    prcent = float(remaining)/float(limit)*100
+
+                if res == "/application/rate_limit_status":
+                    print "post rate limit too low: ", precent, " want: ", self.min_post_rate_limit, " waiting 30 sec"
+                    if precent < self.min_post_rate_limit:
+                        time.sleep(30)
+                        self._check_rate_limit_post()
 
     def retweet(self, tweet):
-        _check_rate_limit_post()
+        self._check_rate_limit_post()
         r = api.request('statuses/retweet/:' + str(tweet.get_id()))
-	CheckError(r)
+        print "retweet to tweed id: ", str(tweet.get_id()), " text: ", tweet.get_text()
+	    self._check_error(r)
 
     def favorite(self, tweet):
-        _check_rate_limit_post()
+        self._check_rate_limit_post()
         r = api.request('favorites/create', {'id': tweet.get_id()})
-	CheckError(r)
+        print "favorite tweet id: ", tweet.get_id()
+	    self._check_error(r)
 
     def serch_tweets(self, serch_query, count):
-        _check_rate_limit_search()
+        self._check_rate_limit_search()
         tweets = []
         r = api.request('search/tweets', {'q':search_query, 'result_type':"mixed", 'count':count})
-	error = CheckError(r)
+	    error = self._check_error(r)
         if not error:
+            print "found ", len(r), " new tweets for ", serch_query, " query"
             for tweet_item in r:
                 tweets.append(Tweet(tweet_item))
         return tweets
