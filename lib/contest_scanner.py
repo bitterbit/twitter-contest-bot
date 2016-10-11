40d08904a179b074edc7076eb05d0869f210af17
Gal Tashma <gtappsdev@gmail.com>
2016-10-11 21:55:52 +0000

    Implemented contest scanner with max time delta

--- lib/contest_scanner.py
+++ lib/contest_scanner.py
@@ -1,25 +1,45 @@
+from datetime import datetime, timedelta
+
 class ContestScanner:
 
 	MAX_RESULTS = 100
+	MAX_TIME_DELTA_DAYS = 10
 
 	def __init__(self, api):
 		self.api = api
-		self.scanned_tweets = []
 		self.scanned_ids = dict()
 		
+	def scannAll(self, queries):
+		results = []
+		for query in queries:
+			results.append(self.scan(query))
+			
+		return results 
+		
 	def scan(self, query):
+		results = []
+		
 		tweets = self.api.serch_tweets(query, self.MAX_RESULTS)
-
 		for tweet in tweets:
 			if not self._exists(tweet) and self._is_valid(tweet):
 				self._add(tweet)
+				results.append(tweet)
+					
+		return results
 				
 	def _add(self, tweet):
-		self.scanned_tweets.append(tweet)
 		self.scanned_ids[tweet.get_id()] = True
 	
 	def _exists(self, tweet):
-		pass
+		if tweet.get_id() in self.scanned_ids:
+			return True
+		return False
 	
 	def _is_valid(self, tweet):
-		pass
+		if tweet.retweet_count <= 0:
+			return False
+			
+		if datetime.now() - tweet.created_at > timedelta(days=self.MAX_TIME_DELTA_DAYS):
+			return False
+			
+		return True
