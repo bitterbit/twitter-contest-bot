d00344b3d2c888b92faa2e851ceca06a18e80021
Gal Tashma <gtappsdev@gmail.com>
2016-10-11 20:11:45 +0000

    Added draft interface safe_twitter.py

---
+++ lib/safe_twitter.py
@@ -0,0 +1,15 @@
+from TwitterAPI import TwitterAPI
+
+class SafeTwitter(object):
+    
+    def Retweet(self, tweet):
+        """ Returns status """
+        pass
+
+    def Favorite(self, tweet):
+        """ Returns status """
+        pass
+
+    def Query(self, serch_query, count):
+        """ Returns []lib.tweet """
+        pass
